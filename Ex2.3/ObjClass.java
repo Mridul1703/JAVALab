6c372faa6a80486ae62393fba4dd93cc1b9c6d0d
Mridul Vasudeva <88312870+Mridul1703@users.noreply.github.com>
2022-08-27 12:29:51 +0530

    Add files via upload

---
+++ Ex2.3/ObjClass.java
@@ -0,0 +1,20 @@
+package Lab2;
+
+public class ObjClass {
+	public static void main(String args[]) {
+		Vehicle v = new Vehicle(2000.5, "Black");
+		
+		System.out.println(v.weight);
+		System.out.println(v.color);
+	}
+}
+
+class Vehicle {
+	double weight;
+	String color;
+	
+	Vehicle(double d, String c) {
+		this.weight = d;
+		this.color = c;
+	}
+}
