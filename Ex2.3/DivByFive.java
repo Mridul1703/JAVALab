33a2c393a5a10b7a39dad08eb634cbd802cc9af4
Mridul Vasudeva <88312870+Mridul1703@users.noreply.github.com>
2022-09-03 12:00:52 +0530

    Ex2.3 Q10

---
+++ Ex2.3/DivByFive.java
@@ -0,0 +1,14 @@
+public class DivByFive
+{
+	public static void main(String[] args) {
+		int sum = 0;
+		
+		for(int i=40; i<=250; i++) {
+		    if(i%5==0) {
+		        sum += i;
+		    }
+		}
+		
+		System.out.println("Sum: " + sum);
+	}
+}
